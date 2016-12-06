ab03814942feeb87bc72a3aefef948c3b6da96be
iproduct <office@iproduct.org>
2016-12-06 14:55:28 +0200

    ReactorFluxMethods demo added

---
+++ reactor-demos/src/main/java/demos/ReactorFluxMethods.java
@@ -0,0 +1,29 @@
+package demos;
+
+import java.util.Arrays;
+
+import reactor.core.publisher.BlockingSink;
+import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+public class ReactorFluxMethods {
+
+	public ReactorFluxMethods() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String... args) throws InterruptedException {
+		Flux.fromIterable(Arrays.asList(1L,2L,3L,0L,5L,6L,7L,8L,9L))
+	    .mergeWith(Flux.intervalMillis(100))
+	    .doOnNext(d -> System.out.println("doOnNext(" + d + ")"))
+	    .map( d -> 100 / d)
+	    .take(5)
+	    .onErrorResumeWith(error -> { System.out.println("onErrorResumeWith(" + error + ")"); return Mono.just(0L);} )
+	    .doAfterTerminate(() -> System.out.println("doAfterTerminate()"))
+	    .subscribe(System.out::println);
+		
+		Thread.sleep(3000);
+	}
+}
